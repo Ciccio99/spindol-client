deada875d732b14b8670a622d45ac3c3537d8426
Alberto Scicali <alberto@Big-Bear-Pro.local>
2020-05-04 05:14:43 -0500

    Changed copyright link on login page to sleepwell.ai

--- src/screens/signInScreen/SignInScreen.js
+++ src/screens/signInScreen/SignInScreen.js
@@ -22,7 +22,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://sleepwell.ai/">
         Sleepwell.ai
       </Link>{' '}
       {new Date().getFullYear()}
