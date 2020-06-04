ae2eb74c32b91c41eb6434ed874f0aa0ad647f14
Alberto Scicali <alberto@bigbear.local>
2020-06-04 11:01:59 -0500

    testing

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,9 @@
 export default {
   api_uri: process.env.REACT_APP_API_URI,
-  oauth2: {
-    api_uri: process.env.REACT_APP_API_URI,
-    oura: {
-      client_id: process.env.OURA_CLIENT_ID,
-    },
-  },
   ga: {
-    trackingId: process.env.GA_TRACKING_ID,
+    trackingId: process.env.REACT_APP_GA_TRACKING_ID,
   },
   hotjar: {
-    trackingId: process.env.HOTJAR_TRACKING_ID,
+    trackingId: process.env.REACT_APP_HOTJAR_TRACKING_ID,
   },
 };
