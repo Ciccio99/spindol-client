f10544496a79daec6f44a672768c60d3dbef0fc4
Alberto Scicali <alberto@bigbear.local>
2020-06-04 11:09:56 -0500

    testing

--- src/index.jsx
+++ src/index.jsx
@@ -9,17 +9,13 @@ import HypnosTheme from './constants/HypnosTheme';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-console.log(config);
 if (config.ga.trackingId) {
   ReactGA.initialize(config.ga.trackingId);
-  console.log('GA enabled');
 } else {
   ReactGA.initialize('foo', { testMode: true });
-  console.log('GA TEST enabled');
 }
 if (config.hotjar.trackingId) {
   hotjar.initialize(config.hotjar.trackingId);
-  console.log('hotjar enabled');
 }
 
 ReactDOM.render(
