5189a1bafebd83374c2b5c7929c390d36c53e3cb
Alberto Scicali <alberto@bigbear.local>
2020-07-19 16:48:39 -0500

    Added hoold return functions to prevent changing state while switching daily diary dates

--- src/components/dailyDiaryDetailsPanel/sleepSummaryPanel/SleepSummaryPanel.jsx
+++ src/components/dailyDiaryDetailsPanel/sleepSummaryPanel/SleepSummaryPanel.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import {
   Box,
   Grid,
-  Typography,
 } from '@material-ui/core';
 import StatCard from '../../statCar/StatCard';
 import SleepSummaryServices from '../../../services/SleepSummaryServices';
