a111b170b7606aa606340d07c5695a5f91d5855b
Alberto Scicali <alberto@bigbear.local>
2020-07-27 16:20:53 -0500

    Simplified edit tags modal logic, fixed bug where creating new tag deselected the tags you chose

--- src/components/modals/EditTagsModal.jsx
+++ src/components/modals/EditTagsModal.jsx
@@ -17,55 +17,38 @@ import { useAlertSystemDispatch } from 'context/alertSystemContext';
 import UserServices from 'services/UserServices';
 import styles from './Modals.module.css';
 
+const getAvailableTags = (customTags = []) => {
+  const tagSet = Array.from(new Set(defaultTags.concat(customTags)));
+  tagSet.sort();
+  return tagSet;
+}
+
 const EditTagsModal = ({ open, tags, handleModal, handleSaveTags }) => {
   const user = useUserState();
   const dispatchUser = useUserDispatch();
   const dispatchAlertSystem = useAlertSystemDispatch();
-  const [selectedTags, setSelectedTags] = useState(null);
+  const availableTags = getAvailableTags(user.settings?.tags);
+  const [selectedTags, setSelectedTags] = useState(tags);
   const [tagInput, setTagInput] = useState('');
 
-  useEffect(() => {
-    let tagSet = Array.from(new Set(defaultTags.concat(user?.settings?.tags || [])));
-    tagSet.sort();
-    const tagMap = {};
-    tagSet.forEach((tag) => {
-      tagMap[tag] = {
-        value: tag,
-        selected: tags?.includes(tag) ? true : false,
-      };
-    })
-    setSelectedTags(tagMap);
-  }, [tags, user]);
-
-  const handleTagSelect = (tagValue) => {
-    setSelectedTags(prevState => ({
-      ...prevState,
-      [tagValue]: { value: tagValue, selected: true }
-    }));
+  const handleTagSelect = (tag) => {
+    setSelectedTags(prevState => ([...prevState, tag]));
   };
 
-  const handleTagDeSelect = (tagValue) => {
-    setSelectedTags(prevState => ({
-      ...prevState,
-      [tagValue]: { value: tagValue, selected: false }
-    }));
+  const handleTagDeSelect = (removeTag) => {
+    setSelectedTags(prevState => prevState.filter(tag => tag !== removeTag));
   }
 
   const handleOnModalClose = () => {
-    const tagsToSave = Object.keys(selectedTags)
-      .filter((key) => selectedTags[key].selected)
-      .map((key) => {
-      return selectedTags[key].value;
-    });
-    if (tagsToSave.length === tags.length) {
-      for (let i = 0; i < tagsToSave.length; i += 1) {
-        if (!tags.includes(tagsToSave[i])) {
-          handleSaveTags(tagsToSave);
+    if (selectedTags.length === tags.length) {
+      for (let i = 0; i < selectedTags.length; i += 1) {
+        if (!tags.includes(selectedTags[i])) {
+          handleSaveTags(selectedTags);
           break;
         }
       }
     } else {
-      handleSaveTags(tagsToSave);
+      handleSaveTags(selectedTags);
     }
 
     handleModal(false);
@@ -120,63 +103,66 @@ const EditTagsModal = ({ open, tags, handleModal, handleSaveTags }) => {
       disableRestoreFocus
     >
       <Slide direction="up" in={open}>
-        <Box m={1} display="flex" justifyContent="center" maxWidth="600px" style={{ outline: 0 }}>
+           <Box m={1} display="flex" justifyContent="center" maxWidth="600px" style={{ outline: 0 }}>
           <Paper>
             <Box>
               <Box display="flex" justifyContent="space-between" px={2} pt={2}>
                 <Typography variant="h6">Edit Day Tags</Typography>
                 <CancelOutlinedIcon color="action" className={styles.closeButton} onClick={handleOnModalClose} />
               </Box>
-              <Box p={3} pt={0} pb={4} display="flex" justifyContent="space-between" flexDirection="column" alignItems="center">
-                <Box mt={4}>
+              <Box p={4} display="flex" justifyContent="space-between" flexDirection="column" alignItems="center">
                   <Grid container justify="center" alignItems="center" spacing={2}>
-                    {Object.keys(selectedTags || {}).map((key) => (
-                      <Grid key={key} item >
-                      { selectedTags[key].selected
-                        ? <Chip
-                          label={selectedTags[key].value}
-                          color="primary"
-                          variant="default"
-                          onClick={() => { handleTagDeSelect(selectedTags[key].value) }}
-                          clickable
-                        />
-                        :<Chip
-                          label={selectedTags[key].value}
-                          onClick={() => { handleTagSelect(selectedTags[key].value) }}
-                          disableRipple
-                          clickable
-                        />
-                      }
-                      </Grid>
-                    ))}
+                    {
+                      availableTags.map((tag) => {
+                        if (selectedTags.includes(tag)) {
+                          return (
+                            <Grid item key={tag}>
+                              <Chip
+                                label={tag}
+                                color="primary"
+                                variant="default"
+                                onClick={() => { handleTagDeSelect(tag); }}
+                                clickable
+                              />
+                            </Grid>
+                          );
+                        }
+                        return (
+                          <Grid item key={tag}>
+                            <Chip
+                              label={tag}
+                              onClick={() => { handleTagSelect(tag) }}
+                              disableRipple
+                              clickable
+                            />
+                          </Grid>
+                        );
+                      })
+                    }
                   </Grid>
-                  <Box mt={4}>
-                    <form onSubmit={handleTagSubmit} autoComplete="off">
-                      <Grid container spacing={2} alignItems="center">
-                        <Grid item xs={12} sm={8}>
-                          <TextField
-                            value={tagInput}
-                            name="tagInput"
-                            type="text"
-                            label="New Tag"
-                            placeholder="New Tag"
-                            onChange={(e) => {setTagInput(e.target.value)}}
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                          >
-                          </TextField>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                          <Button disabled={!tagInput} type="submit" color="secondary" variant="contained" disableElevation size="large" fullWidth>
-                            <Typography variant="body2">Add Tag</Typography>
-                          </Button>
-                        </Grid>
+                  <Box mt={4} component="form" onSubmit={handleTagSubmit} autoComplete="off" minWidth="100%">
+                    <Grid container spacing={2} alignItems="center">
+                      <Grid item xs={12} sm={8}>
+                        <TextField
+                          value={tagInput}
+                          name="tagInput"
+                          type="text"
+                          label="New Tag"
+                          placeholder="New Tag"
+                          onChange={(e) => {setTagInput(e.target.value)}}
+                          fullWidth
+                          variant="outlined"
+                          size="small"
+                        >
+                        </TextField>
                       </Grid>
-                    </form>
+                      <Grid item xs={12} sm={4}>
+                        <Button disabled={!tagInput} type="submit" color="secondary" variant="contained" disableElevation size="large" fullWidth>
+                          <Typography variant="body2">Add Tag</Typography>
+                        </Button>
+                      </Grid>
+                    </Grid>
                   </Box>
-                </Box>
-                <Box />
               </Box>
             </Box>
           </Paper>
