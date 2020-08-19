e2a9bcec5b5535bf068b20cb8a8f8e17e3e386a8
Alberto Scicali <alberto.scicali@gmail.com>
2020-08-19 18:00:06 -0500

    headway but better

--- src/components/common/HeadwayWidget.jsx
+++ src/components/common/HeadwayWidget.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import React, { useEffect } from 'react';
 
-const HeadwayWidget = () => (
-  <>
-    <span className="headway-widget" id="headway-widget" />
-    <Helmet>
-      <script async src="https://cdn.headwayapp.co/widget.js" />
-    </Helmet>
-  </>
-);
+const CONFIG = {
+  selector: '.headway-widget',
+  account: 'x8wGEy',
+};
+
+const HeadwayWidget = () => {
+  useEffect(() => {
+    let isMounted = true;
+    const script = document.createElement('script');
+    script.id = 'headway-widget-script';
+    script.async = true;
+    script.src = 'https://cdn.headwayapp.co/widget.js';
+    document.head.appendChild(script);
+    script.onload = () => {
+      if (isMounted) {
+        document.getElementById('headway-widget').innerHTML = '';
+        window.Headway.init(CONFIG);
+      }
+    };
+
+    return () => {
+      isMounted = false;
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <>
+      <span className="headway-widget" id="headway-widget" />
+    </>
+  );
+};
 
 export default HeadwayWidget;
